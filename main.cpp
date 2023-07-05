273afee6af8de9ab6c3f01a0e34e7e3482f776f8
MouhamedBourouba <mouhamedmobiledev@gmail.com>
2023-07-05 13:36:10 +0100

    bind socket and listen on it

--- main.cpp
+++ main.cpp
@@ -14,15 +14,35 @@ int main() {
 
   // create soket
   int soketfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (soketfd < 0) {
+    cerr << "error creating soket error code: " << soketfd << endl;
+    return 1;
+  }
 
-  if (soketfd < 0)
-    cout << "error creating soket error code: " << soketfd;
+  cout << "socket created \n";
 
   // bind soket
+  struct sockaddr_in serverAddr {};
+  serverAddr.sin_addr.s_addr = INADDR_ANY;
+  serverAddr.sin_port = htons(SERVER_PORT);
+  serverAddr.sin_family = AF_INET;
+
+  if (bind(soketfd, (const sockaddr *)&serverAddr, sizeof(serverAddr)) < 0) {
+    cerr << "error binding socket" << endl;
+    return 1;
+  };
+
+  cout << "socket binded \n";
 
   // lisne in soket
-
+  if (listen(soketfd, 1) < 0) {
+    cerr << "error listening" << endl;
+    return 1;
+  }
   // acsept client
+  for(;;) {
+        
+  }
 
   // disconect soket
 
