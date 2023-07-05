72d49f42b4c3e655ea2cb06a39b57b8ddf65232c
MouhamedBourouba <mouhamedmobiledev@gmail.com>
2023-07-05 13:19:24 +0100

    soket creation

--- main.cpp
+++ main.cpp
@@ -1,11 +1,30 @@
+#include <arpa/inet.h>
 #include <iostream>
+#include <netinet/in.h>
+#include <string>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define SERVER_PORT 8888
+#define BUFFER_SIZE 1024
 
 using namespace std;
 
 int main() {
 
-  cout << "gg";
+  // create soket
+  int soketfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+  if (soketfd < 0)
+    cout << "error creating soket error code: " << soketfd;
+
+  // bind soket
+
+  // lisne in soket
+
+  // acsept client
+
+  // disconect soket
 
   return 0;
 }
-
